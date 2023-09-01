8bb32a515d1b151f6690b144cdce5c41f2816d4a
Rashed <rashedulhoqueceo@gmail.com>
2023-09-01 20:38:01 +0600

    added blog

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const loadPhTube = async() => {
+const loadPhTube = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
     const phTubes = data.data;
@@ -9,30 +9,34 @@ const loadPhTube = async() => {
         <button class="btn focus:bg-[#FF1F3D] w-full" onclick="category('${phTube.category_id}')">${phTube.category}</button>
         `;
         categoryBtn.appendChild(btn);
-    }); 
+    });
 }
 
-const category = async(id) => {
+const category = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
-    const categoriesData = data.data;
+    let categoriesData = data.data;
     console.log(categoriesData);
-    
+
     const tubeContainer = document.getElementById('ph-tube-container');
     tubeContainer.textContent = '';
 
+
     const err = document.getElementById('no-data');
-        if(categoriesData.length === 0){
-            err.classList.remove('hidden');
-        } else {
-            err.classList.add('hidden');
-        }
+    if (categoriesData.length === 0) {
+        err.classList.remove('hidden');
+    } else {
+        err.classList.add('hidden');
+    }
+
+
+
 
     categoriesData.forEach(categoryData => {
         const tubeContainerCard = document.createElement('div');
         tubeContainerCard.innerHTML = `
         <div class="card h-80 bg-base-100 shadow-xl">
-                <figure class="h-40 relative"><img src="${categoryData.thumbnail}" alt="ph-tube"/> <span class="absolute right-2 bottom-2 text-white bg-gray-800 rounded-lg px-2">${categoryData.others.posted_date? `${parseInt((categoryData.others.posted_date) / 3600)}hrs ${Math.floor(((categoryData.others?.posted_date) % 3600) / 60)} min ago` : ''}</span></figure>
+                <figure class="h-40 relative"><img src="${categoryData.thumbnail}" alt="ph-tube"/> <span class="absolute right-2 bottom-2 text-white bg-gray-800 rounded-lg px-2">${categoryData.others.posted_date ? `${parseInt((categoryData.others.posted_date) / 3600)}hrs ${Math.floor(((categoryData.others?.posted_date) % 3600) / 60)} min ago` : ''}</span></figure>
                 <div class="card-body">
                   <div class="flex justify-start gap-5">
                     <div>
@@ -40,14 +44,14 @@ const category = async(id) => {
                     </div>
                     <div> 
                         <h2 class="card-title">${categoryData.title}</h2>
-                        <p>${categoryData.authors[0].profile_name} <span>${categoryData.authors[0].verified? '<i class="fa-solid fa-certificate" style="color: #005cfa;"></i>' : ''}</span></p>
+                        <p>${categoryData.authors[0].profile_name} <span>${categoryData.authors[0].verified ? '<i class="fa-solid fa-certificate" style="color: #005cfa;"></i>' : ''}</span></p>
                         <p>${categoryData.others.views}</p>
                     </div>
                   </div>
                 </div>
         </div>
         `;
-        tubeContainer.appendChild(tubeContainerCard);  
+        tubeContainer.appendChild(tubeContainerCard);
     })
 
 }
@@ -55,3 +59,8 @@ const category = async(id) => {
 loadPhTube();
 category('1000');
 
+
+
+const blog = () => {
+    window.location.href = 'blog.html';
+}
