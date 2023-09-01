e85d50e2abc6ed66b00aa25c18ef03aa3cb782c6
Rashed <rashedulhoqueceo@gmail.com>
2023-09-01 16:01:19 +0600

    added error message

--- js/index.js
+++ js/index.js
@@ -2,12 +2,11 @@ const loadPhTube = async() => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
     const data = await res.json();
     const phTubes = data.data;
-
     const categoryBtn = document.getElementById('category-btn');
     phTubes.forEach(phTube => {
         const btn = document.createElement('div');
         btn.innerHTML = `
-        <button class="btn active:bg-[#FF1F3D] w-full" onclick="category('${phTube.category_id}')">${phTube.category}</button>
+        <button class="btn focus:bg-[#FF1F3D] w-full" onclick="category('${phTube.category_id}')">${phTube.category}</button>
         `;
         categoryBtn.appendChild(btn);
     }); 
@@ -16,16 +15,24 @@ const loadPhTube = async() => {
 const category = async(id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
-    const categoriesData = data.data
+    const categoriesData = data.data;
     console.log(categoriesData);
-
+    
     const tubeContainer = document.getElementById('ph-tube-container');
     tubeContainer.textContent = '';
+
+    const err = document.getElementById('no-data');
+        if(categoriesData.length === 0){
+            err.classList.remove('hidden');
+        } else {
+            err.classList.add('hidden');
+        }
+
     categoriesData.forEach(categoryData => {
         const tubeContainerCard = document.createElement('div');
         tubeContainerCard.innerHTML = `
         <div class="card h-80 bg-base-100 shadow-xl">
-                <figure class="h-40"><img src="${categoryData.thumbnail}" alt="ph-tube"/></figure>
+                <figure class="h-40 relative"><img src="${categoryData.thumbnail}" alt="ph-tube"/> <span class="absolute right-2 bottom-2 text-white bg-gray-800 rounded-lg px-2">${categoryData.others.posted_date? `${parseInt((categoryData.others.posted_date) / 3600)}hrs ${Math.floor(((categoryData.others?.posted_date) % 3600) / 60)} min ago` : ''}</span></figure>
                 <div class="card-body">
                   <div class="flex justify-start gap-5">
                     <div>
@@ -33,15 +40,16 @@ const category = async(id) => {
                     </div>
                     <div> 
                         <h2 class="card-title">${categoryData.title}</h2>
-                        <p>${categoryData.authors[0].profile_name}</p>
+                        <p>${categoryData.authors[0].profile_name} <span>${categoryData.authors[0].verified? '<i class="fa-solid fa-certificate" style="color: #005cfa;"></i>' : ''}</span></p>
                         <p>${categoryData.others.views}</p>
                     </div>
                   </div>
                 </div>
         </div>
         `;
-        tubeContainer.appendChild(tubeContainerCard);
+        tubeContainer.appendChild(tubeContainerCard);  
     })
+
 }
 
 loadPhTube();
